2bc81351db16d2feca2476f13c586cf7cdbd9422
Mark Masulis <mark.masulis@sjsu.edu>
2023-05-13 21:58:51 -0700

    backend works

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
 //connect to mysql db
 var express = require('express');
 var router = require('./router');
-var mysql = require('mysql');
 var http = require('http')
 
 const port = 8080;
 
 var app = express();
+
+app.use((req, res, next) => {
+    console.log(req.query);
+    res.on('finish', () => {
+        console.log(res.statusCode);
+    })
+    next();
+})
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    next();
+})
 
 app.use('/', router);
 
--- src/router.js
+++ src/router.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 //req.query.user
 //req.query.password
-router.get('/publishers', async (req, res) => {
+router.get('/publishers', (req, res) => {
     var connection = mysql.createConnection({
         host: 'mysqldb',
         port: 3306,
@@ -15,20 +15,324 @@ router.get('/publishers', async (req, res) => {
 
     connection.connect((err) => {
         if(err){
-            console.log(err)
-            res.status(400).json({message: 'Could not connect to database.'})
+            console.log(err);
+            res.status(400).json({message: 'Could not connect to database.'});
         }
     })
 
     connection.query('SELECT * FROM PUBLISHER', (error, results, fields) => {
         if(error){
-            res.status(400).json({message: 'Unable to complete query.'})
+            res.status(400).json({message: 'Unable to complete query.'});
         }else{
-            console.log(results);
-            console.log(fields);
-            res.status(200).json({});
+            res.status(200).json(results);
         }
     })
-})
+});
+
+//req.query.user
+//req.query.password
+//req.query.series_name
+//req.query.publisher_name
+router.get('/series', (req, res) => {
+    var connection = mysql.createConnection({
+        host: 'mysqldb',
+        port: 3306,
+        user: req.query.user,
+        password: req.query.password,
+        database: 'BOOKSTORE'
+    });
+
+    connection.connect((err) => {
+        if(err){
+            console.log(err);
+            res.status(400).json({message: 'Could not connect to database.'});
+        }
+    })
+
+    if(req.query.publisher_name && req.query.series_name){
+        connection.query('SELECT * FROM SERIES WHERE PUBLISHER_NAME=? AND SERIES_NAME=?', [req.query.publisher_name, req.query.series_name], (error, results, fields) => {
+            if(error){
+                res.status(400).json({message: 'Unable to complete query.'});
+            }else{
+                res.status(200).json(results);
+            }
+        })
+    }else if(req.query.publisher_name){
+        connection.query('SELECT * FROM SERIES WHERE PUBLISHER_NAME=?', [req.query.publisher_name], (error, results, fields) => {
+            if(error){
+                res.status(400).json({message: 'Unable to complete query.'});
+            }else{
+                res.status(200).json(results);
+            }
+        })
+    }else{
+        connection.query('SELECT * FROM SERIES', (error, results, fields) => {
+            if(error){
+                res.status(400).json({message: 'Unable to complete query.'});
+            }else{
+                res.status(200).json(results);
+            }
+        })
+    }
+});
+
+//req.query.user
+//req.query.password
+//req.query.publisher_name
+//req.query.series_name
+router.get('/magazine_issues', (req, res) => {
+    var connection = mysql.createConnection({
+        host: 'mysqldb',
+        port: 3306,
+        user: req.query.user,
+        password: req.query.password,
+        database: 'BOOKSTORE'
+    });
+
+    connection.connect((err) => {
+        if(err){
+            console.log(err);
+            res.status(400).json({message: 'Could not connect to database.'});
+        }
+    })
+
+    connection.query('SELECT * FROM MAGAZINE_ISSUE WHERE SERIES_NAME=? AND PUBLISHER_NAME=?', [req.query.series_name, req.query.publisher_name], (error, results, fields) => {
+        if(error){
+            res.status(400).json({message: 'Unable to complete query.'});
+        }else{
+            res.status(200).json(results);
+        }
+    })
+});
+
+//req.query.user
+//req.query.password
+//req.query.issue_number
+//req.query.publisher_name
+//req.query.series_name
+router.get('/magazine_issue', (req, res) => {
+    var connection = mysql.createConnection({
+        host: 'mysqldb',
+        port: 3306,
+        user: req.query.user,
+        password: req.query.password,
+        database: 'BOOKSTORE'
+    });
+
+    connection.connect((err) => {
+        if(err){
+            console.log(err);
+            res.status(400).json({message: 'Could not connect to database.'});
+        }
+    })
+
+    connection.query('SELECT * FROM MAGAZINE_ISSUE WHERE SERIES_NAME=? AND PUBLISHER_NAME=? AND ISSUE_NUMBER=?', [req.query.series_name, req.query.publisher_name, req.query.issue_number], (error, results, fields) => {
+        if(error){
+            res.status(400).json({message: 'Unable to complete query.'});
+        }else{
+            res.status(200).json(results);
+        }
+    })
+});
+
+//req.query.user
+//req.query.password
+//req.query.publisher_name
+//req.query.series_name
+//req.query.issue_number
+router.get('/articles_from_issue', (req, res) => {
+    var connection = mysql.createConnection({
+        host: 'mysqldb',
+        port: 3306,
+        user: req.query.user,
+        password: req.query.password,
+        database: 'BOOKSTORE'
+    });
+
+    connection.connect((err) => {
+        if(err){
+            console.log(err);
+            res.status(400).json({message: 'Could not connect to database.'});
+        }
+    })
+
+    connection.query('SELECT * FROM ARTICLE WHERE ISSUE_NUMBER=? AND SERIES_NAME=? AND PUBLISHER_NAME=?', [req.query.issue_number, req.query.series_name, req.query.publisher_name], (error, results, fields) => {
+        if(error){
+            res.status(400).json({message: 'Unable to complete query.'});
+        }else{
+            res.status(200).json(results);
+        }
+    })
+});
+
+//req.query.user
+//req.query.password
+//req.query.writer_id
+router.get('/articles_from_writer', (req, res) => {
+    var connection = mysql.createConnection({
+        host: 'mysqldb',
+        port: 3306,
+        user: req.query.user,
+        password: req.query.password,
+        database: 'BOOKSTORE'
+    });
+
+    connection.connect((err) => {
+        if(err){
+            console.log(err);
+            res.status(400).json({message: 'Could not connect to database.'});
+        }
+    })
+
+    connection.query('SELECT * FROM ARTICLE WHERE WRITER_ID=?', [req.query.writer_id], (error, results, fields) => {
+        if(error){
+            res.status(400).json({message: 'Unable to complete query.'});
+        }else{
+            res.status(200).json(results);
+        }
+    })
+});
+
+//req.query.user
+//req.query.password
+//req.query.writer_id
+router.get('/writer', (req, res) => {
+    var connection = mysql.createConnection({
+        host: 'mysqldb',
+        port: 3306,
+        user: req.query.user,
+        password: req.query.password,
+        database: 'BOOKSTORE'
+    });
+
+    connection.connect((err) => {
+        if(err){
+            console.log(err);
+            res.status(400).json({message: 'Could not connect to database.'});
+        }
+    })
+
+    connection.query('SELECT * FROM WRITER WHERE WRITER_ID=?', [req.query.writer_id], (error, results, fields) => {
+        if(error){
+            res.status(400).json({message: 'Unable to complete query.'});
+        }else{
+            res.status(200).json(results);
+        }
+    })
+});
+
+
+
+//req.query.user
+//req.query.password
+//req.query.writer_id
+//req.query.article_title
+router.get('/article_topics', (req, res) => {
+    var connection = mysql.createConnection({
+        host: 'mysqldb',
+        port: 3306,
+        user: req.query.user,
+        password: req.query.password,
+        database: 'BOOKSTORE'
+    });
+
+    connection.connect((err) => {
+        if(err){
+            console.log(err);
+            res.status(400).json({message: 'Could not connect to database.'});
+        }
+    })
+
+    connection.query('SELECT * FROM ARTICLE_TOPICS WHERE WRITER_ID=? AND ARTICLE_TITLE=?', [req.query.writer_id, req.query.article_title], (error, results, fields) => {
+        if(error){
+            res.status(400).json({message: 'Unable to complete query.'});
+        }else{
+            res.status(200).json(results);
+        }
+    })
+});
+
+//req.query.user
+//req.query.password
+router.get('/customers', (req, res) => {
+    var connection = mysql.createConnection({
+        host: 'mysqldb',
+        port: 3306,
+        user: req.query.user,
+        password: req.query.password,
+        database: 'BOOKSTORE'
+    });
+
+    connection.connect((err) => {
+        if(err){
+            console.log(err);
+            res.status(400).json({message: 'Could not connect to database.'});
+        }
+    })
+
+    connection.query('SELECT * FROM CUSTOMER', (error, results, fields) => {
+        if(error){
+            res.status(400).json({message: 'Unable to complete query.'});
+        }else{
+            res.status(200).json(results);
+        }
+    })
+});
+
+//req.query.user
+//req.query.password
+//req.query.customer_id
+router.get('/customer', (req, res) => {
+    var connection = mysql.createConnection({
+        host: 'mysqldb',
+        port: 3306,
+        user: req.query.user,
+        password: req.query.password,
+        database: 'BOOKSTORE'
+    });
+
+    connection.connect((err) => {
+        if(err){
+            console.log(err);
+            res.status(400).json({message: 'Could not connect to database.'});
+        }
+    })
+
+    connection.query('SELECT * FROM CUSTOMER WHERE CUSTOMER_ID=?', [req.query.customer_id], (error, results, fields) => {
+        if(error){
+            res.status(400).json({message: 'Unable to complete query.'});
+        }else{
+            res.status(200).json(results);
+        }
+    })
+});
+
+//req.query.user
+//req.query.password
+//req.query.customer_id
+router.get('/customer_subscriptions', (req, res) => {
+    var connection = mysql.createConnection({
+        host: 'mysqldb',
+        port: 3306,
+        user: req.query.user,
+        password: req.query.password,
+        database: 'BOOKSTORE'
+    });
+
+    connection.connect((err) => {
+        if(err){
+            console.log(err);
+            res.status(400).json({message: 'Could not connect to database.'});
+        }
+    })
+
+    connection.query('SELECT * FROM MAGAZINE_SUBSCRIPTION WHERE CUSTOMER_ID=?', [req.query.customer_id], (error, results, fields) => {
+        if(error){
+            res.status(400).json({message: 'Unable to complete query.'});
+        }else{
+            res.status(200).json(results);
+        }
+    })
+});
 
 module.exports = router;
